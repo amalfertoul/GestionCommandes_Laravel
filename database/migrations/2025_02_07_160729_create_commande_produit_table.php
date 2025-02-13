3750320bffdb15bde29be19f879b4ae80a535bb8
amalfertoul <142597174+amalfertoul@users.noreply.github.com>
2025-02-13 20:36:29 +0100

    migrations

---
+++ database/migrations/2025_02_07_160729_create_commande_produit_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commande_produit', function (Blueprint $table) {
+            $table->foreignId('commande_id')->constrained()->onDelete('cascade');
+            $table->foreignId('produit_id')->constrained()->onDelete('cascade');
+            $table->integer('qte_cmd');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('commande_produit');
+    }
+};
